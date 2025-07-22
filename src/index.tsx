2bbeb843a6e7e1d0faaafa7670e680a1e9632235
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-07-22 19:59:19 +0900

    fix: MUIO customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles'; 
-import theme from './theme';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './app/MaterialTheme'; 
 import './css/index.css';
+ 
 
 ReactDOM.render(
   <React.StrictMode>
