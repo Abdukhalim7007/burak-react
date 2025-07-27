7373bb8419fb854d0e504ff133aed9b6cbb09b83
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-07-27 18:10:13 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { HomeNavbar } from './components/headers/HomeNavbar';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import { Footer } from './components/footer';
+import React from "react";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HelpPage from "./screens/helpPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
-import { HelpPage } from './screens/helpPage';
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
-  
+
   return (
-      <>
-        {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/">
-            <HomePage/>
-          </Route>
-        </Switch>
-        <Footer />
-      </>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
