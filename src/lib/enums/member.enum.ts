7c5f3440f9c5e7b99bcc4d664448f63080aaa4dd
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-08-21 21:22:33 +0900

    feat: develop AppRootState and HomePage type integrations

---
+++ src/lib/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+    Active = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+    ACTIVE = "ACTIVE",
+}
