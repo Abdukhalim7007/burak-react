f499d8b6596013de27a97f33ed9bc026ebd98823
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-08-24 16:32:29 +0900

    feat: create config & env variable

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -28,11 +28,11 @@ const popularDishesRetriever = createSelector(
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
   const { popularDishes } = useSelector(popularDishesRetriever);
-  // Selector: Store => Data
+ 
+  console.log(process.env.REACT_APP_API_URL);
 
   useEffect(() => {
-    // Backend server data request => DATA
-    // Slice: Data => Store
+   
   }, []);
 
   return (
